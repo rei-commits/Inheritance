26d774694d586d9b5623aa64610a6d98df200f3c
Andreina <125166401+rei-commits@users.noreply.github.com>
2024-11-13 15:34:15 -0500

    initialized

---
+++ src/Hospital.java
@@ -0,0 +1,9 @@
+
+public class Hospital {
+
+	public static void main(String args[]) {
+	       Surgeon s = new Surgeon();
+	       s.Doctor_Details();
+	       s.Surgeon_Details();
+	   }
+	}
---
+++ src/Surgeon.java
@@ -0,0 +1,8 @@
+
+public class Surgeon extends Doctor {
+   void Surgeon_Details() {
+       System.out.println("Surgeon  Detail...");
+       System.out.println(Department = "Cardio");
+   }
+}
+
